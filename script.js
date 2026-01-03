8c44fad190332b0d0f17030912956fb0eadeb345
TharunBabu127 <tharunharini2007@gmail.com>
2026-01-03 13:54:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+let data;
+
+fetch("data.json")
+  .then(response => response.json())
+  .then(json => data = json);
+
 function showCountry() {
   const country = document.getElementById("country").value;
-  document.getElementById("output").innerText =
-    "You selected: " + country;
+  const info = data[country];
+
+  document.getElementById("output").innerHTML =
+    `<b>Capital:</b> ${info.capital}<br>
+     <b>Currency:</b> ${info.currency}<br>
+     <b>Best Time:</b> ${info.bestTime}<br>
+     <b>Famous Places:</b> ${info.places.join(", ")}`;
 }
